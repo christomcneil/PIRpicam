33fc45feff85cd04f825222ad79a5644d0f9476c
Chris <christomcneil@gmail.com>
2021-03-26 20:06:28 +0000

    3rd commit

--- pirpicam.py
+++ pirpicam.py
@@ -4,6 +4,7 @@ from time import sleep
 from picamera import PiCamera
 from datetime import datetime
 import os
+import sys
 
 recordFlag = False
 
@@ -17,13 +18,14 @@ GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 camera = PiCamera(resolution=(2592,1944), framerate=10)
 # average out reading from motion sensor
 
-
+print("setup done")
 try:
 
    while True: # Run forever
         filename = "{0:%m}-{0:%d}-{0:%H}-{0:%M}-{0:%S}.h264".format(datetime.now())
         if GPIO.input(sensorPin) == GPIO.HIGH:
             recordFlag = True
+	    print("motion detected")
             camera.start_recording(filename, format='h264', quality=23)
             lastTime = time.time()
             while recordFlag == True:
@@ -40,7 +42,7 @@ try:
             camera.stop_recording()
             #print("recording finished!")
             os.system('mv /home/pi/camCapture/*.h264 /media/usb/')
-            #print("!!!!!!!!!!!!!!!!  ALL DONE  !!!!!!!!!!!!!!!!!")
+            print("!!!!!!!!!!!!!!!!  ALL DONE  !!!!!!!!!!!!!!!!!")
 except (KeyboardInterrupt, SystemExit):
     print '\n! Received keyboard interrupt, quitting threads.\n'
     sys.exit()
