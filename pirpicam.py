543aa2692ebe0bec57c5b8c21827dd33d2dc2050
Chris <christomcneil@gmail.com>
2021-03-26 20:26:19 +0000

    4th

--- pirpicam.py
+++ pirpicam.py
@@ -6,43 +6,30 @@ from datetime import datetime
 import os
 import sys
 
-recordFlag = False
 
-sensorDelay = 0 
-lastTime = 0
-thisTime = 0
 sensorPin = 18
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-camera = PiCamera(resolution=(2592,1944), framerate=10)
-# average out reading from motion sensor
+camera = PiCamera()
+camera.resolution(2592,1944) 
+
+
+def capture_still(): 
+       camera.capture(filename) 
 
 print("setup done")
 try:
 
-   while True: # Run forever
-        filename = "{0:%m}-{0:%d}-{0:%H}-{0:%M}-{0:%S}.h264".format(datetime.now())
+    while True: # Run forever
+        filename = "{0:%m}-{0:%d}-{0:%H}-{0:%M}-{0:%S}.jpg".format(datetime.now())
         if GPIO.input(sensorPin) == GPIO.HIGH:
-            recordFlag = True
-	    print("motion detected")
-            camera.start_recording(filename, format='h264', quality=23)
-            lastTime = time.time()
-            while recordFlag == True:
-                if GPIO.input(sensorPin) == GPIO.LOW:
-                    thisTime = int(time.time() - lastTime)
-                    #print("!!! SENSOR IS OFF !!! current time limit is "), thisTime 
-                    sleep(.1) # buffering time. video recording gets choppy without it
-                    if thisTime > sensorDelay:
-                        recordFlag = False
-                else:
-                    lastTime = time.time()
-                    sleep(1)
-
-            camera.stop_recording()
-            #print("recording finished!")
-            os.system('mv /home/pi/camCapture/*.h264 /media/usb/')
-            print("!!!!!!!!!!!!!!!!  ALL DONE  !!!!!!!!!!!!!!!!!")
+            
+            print("motion detected")
+            sleep(0.1)
+            capture_still()
+            sleep(1)
+            os.system('mv /home/pi/*.jpg /home/pi/motionimages/')
 except (KeyboardInterrupt, SystemExit):
-    print '\n! Received keyboard interrupt, quitting threads.\n'
-    sys.exit()
+    print ('\n! Received keyboard interrupt, quitting\n')
+sys.exit()
